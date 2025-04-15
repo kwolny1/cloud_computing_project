1b718d77f0edf8c810c4534ab9eb0053a73011ce
Kuba Seliga <kubaseli.02@gmail.com>
2025-04-15 21:22:54 +0200

    Initial terraform and lambda function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cloud_computing_project
+1. set up aws account on https://aws.amazon.com
+2. download terraform from https://developer.hashicorp.com/terraform/install
+3. in cmd, go to folder with main.tf and lambda_function.zip
+4. run:
+"your_path_to_downloaded_terraform\terraform.exe" init
+"your_path_to_downloaded_terraform\terraform.exe" plan
+"your_path_to_downloaded_terraform\terraform.exe" apply
+enter 'yes' value when asked
+5. check aws webpage if stuff that .tf code should do is done (Lambda->Functions, DynamoDB->Tables, API Gateway->APIs)
---
+++ main.tf
@@ -0,0 +1,116 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_dynamodb_comprehend_policy"
+  description = "Allow Lambda to use DynamoDB and Comprehend"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "dynamodb:PutItem"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "comprehend:DetectKeyPhrases"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_dynamodb_table" "study_notes" {
+  name         = "StudyNotes"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "note_id"
+
+  attribute {
+    name = "note_id"
+    type = "S"
+  }
+}
+
+resource "aws_lambda_function" "summarize_notes_lambda" {
+  function_name = "summarize_notes_lambda"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  filename      = "lambda_function.zip"
+  timeout       = 10
+
+  environment {
+    variables = {
+      DYNAMO_TABLE = aws_dynamodb_table.study_notes.name
+    }
+  }
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "notes-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.summarize_notes_lambda.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /summarize"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.summarize_notes_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
